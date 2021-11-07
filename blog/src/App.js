2b7aaaaadbaeb8658299b9f1b5b572b20e9895f3
cos <junzerokim@gmail.com>
2021-11-07 22:09:22 +0900

    REACT 3강

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,18 +1,30 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
 
-  let posts = '판교 맛집 리스트'
+  let [글제목, 글제목변경] = useState(['남자 코트 추천', '판교 대박 맛집', '카카오커머스']);
 
+  let posts = '판교 맛집 리스트'
   return (
     <div className="App">
       <div className="black-nav">
         <div>Dev Blog</div>
       </div>
       <div className="list">
-        <h4> { posts } </h4>
-        <p>test</p>
+        <h4> { 글제목[0] } </h4>
+        <p>11월 7일 발행</p>
+        <hr/>
+      </div>
+      <div className="list">
+        <h4> { 글제목[1] } </h4>
+        <p>11월 7일 발행</p>
+        <hr/>
+      </div>
+      <div className="list">
+        <h4> { 글제목[2] } </h4>
+        <p>11월 7일 발행</p>
         <hr/>
       </div>
     </div>
